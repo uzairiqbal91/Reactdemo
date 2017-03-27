4a3fd61768fa621421c24cf37110feb8d72f8852
mirza umer <mirzaumersaleem@yahoo.com>
2017-03-27 17:02:32 +0500

    Table component add

---
+++ src/OrderList.js
@@ -0,0 +1,23 @@
+
+
+import React, {component} from 'react';
+class OrderList extends component{
+render(){
+return (
+<ol>
+        <li> Semester 1 </li>
+        <li> Semester 2 </li>
+        <li>Semester 3 </li>
+        <li>Semester  4</li>
+        <li> Semester 5</li>
+        <li> Semester 6</li>
+        <li> Semester 7</li>
+        <li> Semester 8</li>
+ </ol>
+);
+
+}
+
+}
+
+export default OrderList;
---
+++ src/un_Order_List.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class un_Order_List extends Component{
+render()
+{
+  return (
+      <ul>
+          <li>umer</li>
+          <li>react_01</li>
+          <li>learning </li>
+       </ul>    
+ );
+}
+}
+export default un_Order_List;
